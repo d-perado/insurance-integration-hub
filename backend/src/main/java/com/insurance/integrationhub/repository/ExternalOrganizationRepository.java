0f51f7b20e58ae4d1f2f8d500e57b45482a19c9d
d-perado <sjm01065@naver.com>
2026-04-25 22:21:25 +0900

    feat: 외부기관 기관명 중복 체크 기능 추가

--- backend/src/main/java/com/insurance/integrationhub/repository/ExternalOrganizationRepository.java
+++ backend/src/main/java/com/insurance/integrationhub/repository/ExternalOrganizationRepository.java
@@ -4,4 +4,6 @@ import com.insurance.integrationhub.domain.ExternalOrganization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExternalOrganizationRepository extends JpaRepository<ExternalOrganization, Long> {
+
+    boolean existsByName(String name);
 }
